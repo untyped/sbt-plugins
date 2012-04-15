6c9ea61c7701e609fb4474e305a34cd6ce1c15fa
Dave Gurnell <dave@untyped.com>
2012-04-15 19:26:42 +0100

    Added extra (currently failing) test case for sbt-tipi

--- sbt-tipi/src/sbt-test/sbt-tipi/import/project/Echo.scala
+++ sbt-tipi/src/sbt-test/sbt-tipi/import/project/Echo.scala
@@ -3,9 +3,9 @@ package project
 import tipi.core._
 import tipi.core.Implicits._
 
-class Echo extends Env(Map(
+class Echo extends Env(
   "echo" -> Transform.Simple {
     case Block(_, args, _) =>
       Text(args.get[String]("text").getOrElse("<no 'text' argument>"))
   }
-))
+)
