566f80da70a65e322a2443c9a0eebbf2b1a04e7d
Dave Gurnell <dave@untyped.com>
2012-07-09 12:03:32 +0100

    Fix typo

--- project/Build.scala
+++ project/Build.scala
@@ -22,7 +22,7 @@ object Build extends Build {
       case "0.11.1" => "com.github.siasia" %% "xsbt-web-plugin" % "0.11.1-0.2.10"
       case "0.11.2" => "com.github.siasia" %% "xsbt-web-plugin" % "0.11.2-0.2.11"
       case "0.11.3" => "com.github.siasia" %% "xsbt-web-plugin" % "0.11.3-0.2.11.1"
-      case other    => throw new Exception("Build.scala: don't know what vers of xsbt-web-plugin to use for SBT " + other)
+      case other    => throw new Exception("Build.scala: don't know what version of xsbt-web-plugin to use for SBT " + other)
     }
 
   // Settings -----------------------------------
--- project/plugins.sbt
+++ project/plugins.sbt
@@ -4,6 +4,6 @@ libraryDependencies <+= sbtVersion { v =>
     case "0.11.1" => "org.scala-tools.sbt" %% "scripted-plugin" % v
     case "0.11.2" => "org.scala-tools.sbt" %% "scripted-plugin" % v
     case "0.11.3" => "org.scala-sbt"       %% "scripted-plugin" % v
-    case other    => throw new Exception("plugins.sbt: don't know what vers of scripted-plugin to use for SBT " + other)
+    case other    => throw new Exception("plugins.sbt: don't know what version of scripted-plugin to use for SBT " + other)
   }
 }
